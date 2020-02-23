4cfc08a5e86b1faac5dba93f54d2a36c8ca55297
Krystian Reinke <kryscreasy@gmail.com>
2020-02-23 12:42:43 +0100

    initial commit

---
+++ src/main/java/com/pomoge/pbm/PbmApplication.java
@@ -0,0 +1,13 @@
+package com.pomoge.pbm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PbmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pomoge/pbm/PbmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pomoge.pbm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
